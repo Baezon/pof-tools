46cfc103462ca5f1d70192d26e5e8712f53741e5
Mario Carneiro <di.gama@gmail.com>
2022-04-05 21:29:33 -0700

    include vertex offsets in bbox calculation (#45)
    fixes #43

--- src/ui.rs
+++ src/ui.rs
@@ -1292,8 +1292,9 @@ impl PofToolsGui {
                     continue;
                 }
 
+                let offset = model.get_total_subobj_offset(subobj.obj_id);
                 for vert in &subobj.bsp_data.verts {
-                    if !model.header.bbox.contains(*vert) {
+                    if !model.header.bbox.contains(offset + *vert) {
                         return true;
                     }
                 }
